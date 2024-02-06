6f192d0eb52bf01a28e42efaac976a1c0b1d20ff
Martin Nordholts <martin.nordholts@codetale.se>
2024-02-06 16:59:25 +0100

    wip  2024-02-06T16:59:25+01:00

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn process_binary(path: &Path) -> Result<TreemapData> {
 
     let mut treemap_data = TreemapData {
         name: path.to_string_lossy().to_string(),
-        sum: 0,
+        value: 0,
         children: HashMap::new(),
     };
 
@@ -53,7 +53,7 @@ fn process_binary(path: &Path) -> Result<TreemapData> {
             // The root is a special case so manually increment the size here.
             // Note that we only care about bytes that have an associated debug
             // info location so we can map it.
-            treemap_data.sum += 1;
+            treemap_data.value += 1;
 
             let mut current = &mut treemap_data;
             if let Some(file) = loc.file {
@@ -82,7 +82,7 @@ pub struct TreemapData {
 
     /// The size in bytes of this node. This is the sum of the sizes of all its
     /// children. We call the field `sum` for easy interopability with d3js.
-    pub sum: u64,
+    pub value: u64,
 
     /// How the `size` is distributed among the children.
     #[serde(serialize_with = "hash_map_values_to_vec")]
@@ -106,10 +106,10 @@ impl TreemapData {
             .entry(key.to_string())
             .or_insert_with(|| TreemapData {
                 name: key.to_string(),
-                sum: 0,
+                value: 0,
                 children: HashMap::new(),
             });
-        child.sum += 1;
+        child.value += 1;
         child
     }
 }
